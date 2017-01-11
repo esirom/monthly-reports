90d969cbbdf16950ecc8d33c7732680135a28b21
Leon <wrightleon@icloud.com>
2017-01-11 10:34:54 -0500

    divided pages into components, placed them in separate files in the content folder

---
+++ app/content/dashboard.php
@@ -0,0 +1,10 @@
+<div class="content-wrapper">
+   <h3>Page title
+      <small>Subtitle</small>
+   </h3>
+   <div class="row">
+      <div class="col-lg-12">
+         <p>A row with content</p>
+      </div>
+   </div>
+</div>
