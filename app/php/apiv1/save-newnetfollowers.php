0f613636f6d3e4cb612b129569f1db0981a3b76b
Leon <wrightleon@icloud.com>
2017-01-13 14:10:03 -0500

    Updated query to store month and year of report data separately and not just as a timestamp

--- app/php/apiv1/save-newnetfollowers.php
+++ app/php/apiv1/save-newnetfollowers.php
@@ -4,8 +4,10 @@
   $brand_id = $_POST['brandId'];
   $likes = $_POST['facebookLikes'];
   $followers = $_POST['instagramFollowers'];
+  $month = $_POST['month'];
+  $year = $_POST['year'];
 
-  $query = "INSERT INTO monthly_followers (no_of_followers, no_of_likes, brand_id) VALUES ('{$followers}', '{$likes}', '{$brand_id}')";
+  $query = "INSERT INTO monthly_followers (no_of_followers, no_of_likes, brand_id, month, year) VALUES ('{$followers}', '{$likes}', '{$brand_id}', '{$month}', '{$year}')";
 
   if ($connection->query($query)) {
     echo "Saved";
