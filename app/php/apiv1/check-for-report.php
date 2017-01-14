59d0ceb3b218210165053617ac9652ac9c6c6b0e
Leon <wrightleon@icloud.com>
2017-01-14 18:02:41 -0500

    Fixed the way json objects are outputted to accommodate for objects that contain all the data for the form ar once.

--- app/php/apiv1/check-for-report.php
+++ app/php/apiv1/check-for-report.php
@@ -1,24 +1,24 @@
 <?php
-  require_once '../connection.php';
+    require_once '../connection.php';
 
-  $month = $_GET['month'];
-  $year = $_GET['year'];
-  $brand = $_GET['brandId'];
+    $month = $_GET['month'];
+    $year = $_GET['year'];
+    $brand = $_GET['brandId'];
 
-  $fb_engagement_sql = "SELECT * FROM monthly_followers WHERE brand_id='{$brand}' AND month='{$month}' AND year='{$year}'";
+    $fb_engagement_sql = "SELECT * FROM monthly_followers WHERE brand_id='{$brand}' AND month='{$month}' AND year='{$year}'";
 
-  $fb_result = $connection->query($fb_engagement_sql);
+    $fb_result = $connection->query($fb_engagement_sql);
 
-  if ($fb_result->num_rows > 0) {
-    $data = $fb_result->fetch_all(MYSQLI_ASSOC);
+    $data = array();
+    if ($fb_result->num_rows > 0) {
+        $stats = $fb_result->fetch_assoc();
+
+        $data['stats'] = array('id'=>$stats['id'], 'no_of_likes'=>$stats['no_of_likes'], 'no_of_followers'=>$stats['no_of_followers']);
+
+    } else {
+        echo "failed";
+    }
+
     header('Content-type: application/json');
+    echo json_encode($data);
 
-    $to_strip = array("[", "]");
-    $unstripped_json = json_encode($data);
-
-    $stripped_json = str_replace($to_strip, "",$unstripped_json);
-    print $stripped_json;
-  } else {
-    echo "failed";
-  }
-?>
