8b9cc54d0053dab4e4be58eea984b9bbcd171c39
Leon <wrightleon@icloud.com>
2017-01-14 19:02:47 -0500

    Data for previous month is now outputted in the json object.

--- app/php/apiv1/check-for-report.php
+++ app/php/apiv1/check-for-report.php
@@ -1,5 +1,6 @@
 <?php
     require_once '../connection.php';
+    require_once 'functions/date-functions.inc';
 
     $month = $_GET['month'];
     $year = $_GET['year'];
@@ -15,8 +16,19 @@
 
         $data['stats'] = array('id'=>$stats['id'], 'no_of_likes'=>$stats['no_of_likes'], 'no_of_followers'=>$stats['no_of_followers']);
 
-    } else {
-        echo "failed";
+    }
+
+    //Checking for data from previous months.
+    $last_month_date = checkForPrevious($month, $year);
+
+    $last_month_engagement_sql = "SELECT * FROM monthly_followers WHERE brand_id='{$brand}' AND month='{$last_month_date["last_month"]}' AND year='{$last_month_date["last_year"]}'";
+
+    $last_month_engagement_result = $connection->query($last_month_engagement_sql);
+
+    if ($last_month_engagement_result->num_rows > 0) {
+        $last_month_stats = $last_month_engagement_result->fetch_assoc();
+
+        $data['last_month_stats'] = array('id'=>$last_month_stats['id'], 'no_of_likes'=>$last_month_stats['no_of_likes'], 'no_of_followers'=>$last_month_stats['no_of_followers']);
     }
 
     header('Content-type: application/json');
